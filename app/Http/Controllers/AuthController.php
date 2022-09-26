099725b6c2ead641502380619ad296b6b768aac3
DonaSoft <donasoft.team@gmail.com>
2022-09-26 02:49:25 -0400

    Test Routes & add Player Resources

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,4 +8,18 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     use HttpResponses;
+
+    public function login(){
+        return "Login Method";
+    }
+
+
+    public function register(){
+        return response()->json(["Register Method"]) ;
+
+    }
+    public function logout(){
+        return response()->json(["logout Method"]) ;
+
+    }
 }
