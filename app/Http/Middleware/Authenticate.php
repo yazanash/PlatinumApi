067646dec7d163e0f594221ec317e69a12ec34d1
DonaSoft <donasoft.team@gmail.com>
2022-09-26 20:50:42 -0400

    Disable Redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\HttpResponses;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    use HttpResponses;
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -14,8 +16,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if( $request->is('api/*')){
+            return $this->error("","Sorry : You don't have permissions :)",403);
+        }
+        else if (! $request->expectsJson()) {
             return route('login');
         }
+
     }
 }
